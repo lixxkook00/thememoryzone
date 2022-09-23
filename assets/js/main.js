1e3441708c0641a6ceb17292d237d7c604f074df
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-09-23 11:05:06 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,35 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
-const toggleNavMobile = () => {
-    $('.soft-menu').classList.toggle('open')
-    $('.soft-menu__list').classList.toggle('open')
-}
-
-$('.soft-menu__list').onclick = (e) => {
-    e.stopPropagation()
-}
-
-$('.scroll-to-top').onclick = () => {
-    window.scrollTo({
-        top: 0,
-        left: 100,
-        behavior: 'smooth'
-    });
-}
+// $('.scroll-to-top').onclick = () => {
+//     window.scrollTo({
+//         top: 0,
+//         left: 100,
+//         behavior: 'smooth'
+//     });
+// }
 
 // loading animation
 window.addEventListener('load', (event) => {
-  $('.loading').style.display = "none"
+  $('.loading-gif').style.display = "none"
+  $('.letter-image').classList.add("active")
 });
 
-// SLIDER
-var swiper1 = new Swiper(".swiper-top-stream", {
-    slidesPerView: 4,
-    spaceBetween: 10,
-    slideToClickedSlide: true,
-    // auto
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: '.swiper-button-custom-next',
-        prevEl: '.swiper-button-custom-prev',
-    },
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        // when window width is >= 480px
-        120: {
-        slidesPerView: 2,
-        spaceBetween: 2
-        },
-        // when window width is >= 640px
-        740: {
-        slidesPerView: 3,
-        spaceBetween: 4
-        },
-        1023: {
-        slidesPerView: 3,
-        spaceBetween: 10
-        }
+$('.letter-content').onclick = () => {
+    $('#loading').classList.add("remove")
+}
+
+window.onscroll = function (e) {  
+    let distanceFromTop = $('.schedule').getBoundingClientRect().top;
+    let heightElement = $('.schedule').clientHeight;
+
+    let thisArea = distanceFromTop+heightElement
+
+    if(distanceFromTop<(window.screen.height/2) && thisArea >0){
+        const thisShitPercent = 100-(distanceFromTop/(window.screen.height/2))*100;
+
+        // console.log(thisShitPercent/2);
+
+        $('.location-car').style.transform = `translateX(-${thisShitPercent/3.1}vw)`
     }
-});
+} 
